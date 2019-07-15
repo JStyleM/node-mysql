16097a950fc8ee99c37813c04cd64fb16dc10db7
Jhonatan Moncada <jhonatan.moncada@icpna.edu.pe>
2019-07-15 14:29:48 -0500

    commit 3

--- src/index.js
+++ src/index.js
@@ -1,13 +1,21 @@
 const express = require('express');
 const morgan = require('morgan');
+const routes = require('./routes/index');
+const exphbs = require('express-handlebars');
+
+
 const app = express();
 
 // SETTINGS
 app.set('port', process.env.PORT || 3000);
+app.engine('.hbs', exphbs({
+    
+}))
 
 
 // MIDDLEWARE
 app.use(morgan('dev'));
+app.use(routes);
 
 
 // SERVER
